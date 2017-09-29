dfd02e7fd15a806c03ebef9f213ff205c3bded2d
Gordon Lippa <gnet2k2@hotmail.com>
2017-09-28 22:51:57 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 setup(name='stateScript',
       version='1.0',
-	  python_requires='>=3',
-	  description='Assessment code.',
+      python_requires='>=3',
+      description='Assessment code.',
       py_modules=['stateScript'],
-	  author='Gordon Lippa',
-	  author_email='gordon.lippa@gmail.com',
-	  url='https://github.com/gblkz/sdetExam',
-	  license="MIT",
+      author='Gordon Lippa',
+      author_email='gordon.lippa@gmail.com',
+      url='https://github.com/gblkz/sdetExam',
+      license="MIT",
+      install_requires=['requests']
       )
