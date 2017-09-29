eda6aba9a7b17ded66062dd881cebb9229183022
Gordon Lippa <gnet2k2@hotmail.com>
2017-09-28 20:38:29 -0700

    Added readme.rst

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='stateScript',
+      version='1.0',
+	  description='Assessment code.',
+      py_modules=['stateScript'],
+	  author='Gordon Lippa',
+	  author_email='gordon.lippa@gmail.com',
+	  url='https://github.com/gblkz/sdetExam',
+	  license="MIT",
+      )
