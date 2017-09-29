f799eca6282b2976efda3f58d5c81383e00d12b5
Gordon Lippa <gnet2k2@hotmail.com>
2017-09-28 21:22:47 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 setup(name='stateScript',
       version='1.0',
+	  python_requires='>=3',
 	  description='Assessment code.',
       py_modules=['stateScript'],
 	  author='Gordon Lippa',
